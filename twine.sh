609c07eb0a4ea46b5f6e14f53672bd897d54a568
flipphillips <flip@skidmore.edu>
2016-09-25 20:02:45 -0400

    Moved to a config file based configuration

--- twine.sh
+++ twine.sh
@@ -9,17 +9,36 @@
 WGET="/usr/local/bin/wget"
 DROP="/usr/local/bin/drop_data.sh"
 
-LOGIN="email=YOUR_TWINE_EMAIL_HERE&password=YOUR_TWINE_PW_HERE"
-TWINEID="YOUR_TWINE_ID_HERE"
-DATABIN="YOUR_WOLFRAM_DATABIN_HERE"
+# sets  EMAIL, PASSWORD, DATABIN, and TWINEID
+if [[ -f ./twine.config ]]; then
+    source ./twine.config
+elif [[ -f /usr/local/etc/twine.config ]]; then
+    source /usr/local/etc/twine.config
+elif [[ -f /etc/twine.config ]]; then
+    source /etc/twine.config
+else
+    echo "No configuration found."
+    exit -1
+fi
 
-# login and 
+: "${EMAIL?Need to set EMAIL non-empty}"
+: "${PASSWORD?Need to set PASSWORD non-empty}"
+: "${DATABIN:?Need to set DATABIN non-empty}"
+: "${TWINEID:?Need to set TWINEID non-empty}"
+
+LOGIN="email=$EMAIL&password=$PASSWORD"
+
 $WGET -o /tmp/log.txt --quiet -O /tmp/temp.txt --keep-session-cookies --save-cookies /tmp/cookies.txt --no-check-certificate --post-data=${LOGIN} https://twine.cc/login
 
 $WGET -o /tmp/log.txt --quiet -O /tmp/temp.txt --load-cookies /tmp/cookies.txt --no-check-certificate https://twine.cc/${TWINEID}/rt?cached=1
 
 TEMP=`cat /tmp/temp.txt | awk -F"," '{print $7}' | awk -F"]" '{print $1}' | tr -d ' '`
-let TEMP="TEMP/100"
+TEMP=`bc <<< "scale=2; $TEMP/100"`
 
-$DROP $DATABIN "temperature" $TEMP
+# these are obviously not necessary, but hesful for debugging
+$DROP $DATABIN time `date "+%m%d%H%M%Y.%S"`
+$DROP $DATABIN host `hostname`
+
+$DROP $DATABIN temperature $TEMP
+
 $DROP $DATABIN --send &> /tmp/twine.log
